6dd3351f9b04c0b8e99562cd9f72d89a963942b8
PRANESH SHARMA <22mc3042@rgipt.ac.in>
2025-01-18 02:22:08 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/' , (req,res) => {
+    res.send('response on request ! ');
+
+})
+
+app.listen(3000, () => {
+    console.log('server is running on port 3000');
+}
+
+)
